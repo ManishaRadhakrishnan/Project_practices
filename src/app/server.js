eb6abd0bb8707d2e05788767cc1a46711b065ef2
Jayashankar Jayan <jayashankar.jayan@gmail.com>
2019-03-16 21:50:44 +0530

    Heroku push 0.6

--- src/app/server.js
+++ src/app/server.js
@@ -7,8 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname));
 
-app.get('/*', function(req,res) {
-
+app.get('/', function(req,res) {
+console.log((__dirname);
 res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
